73f74aa6efc3563e8f586bba50435fc35d15bdda
Harun Akdoğan <akdogan_41090@hotmail.com>
2021-05-30 21:21:47 +0300

    boot

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[,] stones1 = new GameObject[5,13];
+    public GameObject[,] stones2 = new GameObject[5, 13];
+    public Vector3 startPoint; 
+    public GameObject stonePrefab;
+    public Sprite [] stonesAll;
+    public SpriteMask mask;
+
     void Start()
     {
-        
+        int index = 0;
+
+        for (int i=0; i<4; i++)
+        {
+            for (int j = 0; j < 13; j++)
+            {
+                
+                stones1[i, j] = Instantiate(stonePrefab, startPoint + new Vector3(j,i*2,0), Quaternion.identity);
+                stones1[i, j].GetComponent<SpriteRenderer>().sprite = stonesAll[index];
+                index++;
+            }
+        }
+
+        index = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 13; j++)
+            {
+
+                stones2[i, j] = Instantiate(stonePrefab, startPoint + new Vector3(j + 20, i * 2, 0), Quaternion.identity);
+                stones2[i, j].GetComponent<SpriteRenderer>().sprite = stonesAll[index];
+                index++;
+            }
+        }
+
+
+
+
     }
 
     // Update is called once per frame
